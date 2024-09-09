05073e87c3f6f61dcee9ee49facc7a71863ad451
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-09-09 20:42:28 +1000

    fixed dockerfile

--- replace-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Replace the placeholder values in the Angular configuration file
-sed -i "s#http://127.0.0.1:8000/api#http://localhost:8000/api#g" /usr/share/nginx/html/assets/config/app-config.json
-
-exec "$@"
